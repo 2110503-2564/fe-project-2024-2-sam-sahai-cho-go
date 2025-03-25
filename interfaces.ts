9dd4c48f9d2ff98412381ff0b05f06edd1ac44c5
Aimeric Supasuth <aimeric1478@gmail.com>
2025-03-25 21:45:08 +0700

    Initial Commit

---
+++ interfaces.ts
@@ -0,0 +1,6 @@
+export interface ReservationItem{
+    _Id: string
+    apptDate:string
+    user: string
+    dentist: string
+}
---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images:{
+        domains:["drive.usercontent.google.com"]
+    },
+    experimental:{
+        serverActions:true
+    }
+};
+
+export default nextConfig;
